e472edf3cef88a57b2a9151790e38b81d578298b
Nahuel Savedra <nsavedra97@gmail.com>
2021-10-05 12:18:29 -0300

    Agrego segundo campo para repetir clave y sesion de errores

--- procesarRegistro.php
+++ procesarRegistro.php
@@ -1,41 +1,21 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <title>TP N*2 - Sanson Ezequiel</title>
-</head>
-<body>
-<?php
-
-$local="localhost";
-$userBase="root";
-$passwordBase="";
-$nombreBase="pw2";
+<?php include('conexion.php');
 
 $usuario = $_POST["usuario"];
 $password= $_POST["clave"];
+$repitePassword= $_POST["repiteClave"];
+$rol = "CLIENTE";
 
-$db = new mysqli($local,$userBase,$passwordBase,$nombreBase);
+if($password == $repitePassword) {
+    $usuarioNuevo = "INSERT INTO usuario (usuario, clave,rol) VALUES (? , ? , '$rol')";
+    $comm = $db->prepare($usuarioNuevo);
 
-if($db->connect_error){
-    echo "Ha ocurrido un error" . $db->connect_error;
+    $comm->bind_param("ss",$usuario, $password); //ss string , ssi integer , ssb bolean
+    $comm->execute();
+}else {
+    header('Location: registro.php');
+    $_SESSION['errores'] = "Las contraseñas no coinciden";
+
+
 }
 
-$usuarioNuevo = "INSERT INTO usuario (usuario, clave) VALUES (? , ?)";
-$comm = $db->prepare($usuarioNuevo);
-
-$comm->bind_param("ss",$usuario, $password); //ss string , ssi integer , ssb bolean
-$comm->execute();
-
 $db->close();
-
-include('./header.php');
-echo "<div class='w3-grey w3-container' style='min-height:500px;'>Se ha registrado correctamente</div>";
-include('./footer.php');
-?>
-
-</body>
-</html>
