21a908c7b337aaecb937974fcbe63d43b6364761
Nahuel Savedra <nsavedra97@gmail.com>
2021-10-05 12:17:45 -0300

    Agrego rol a la tabla usuario y usuario admin

---
+++ bdd.sql
@@ -0,0 +1,11 @@
+use pw2;
+
+-- CREATE TABLE usuario (idusuario SMALLINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+-- 					  usuario VARCHAR(30) NOT NULL,
+--                       rol VARCHAR(15) NOT NULL,
+--                       clave VARCHAR(30) NOT NULL);
+--
+--
+-- INSERT INTO usuario (usuario, clave, rol) VALUES ("admin@admin.com","admin","ADMIN");
+
+SELECT * FROM usuario;
