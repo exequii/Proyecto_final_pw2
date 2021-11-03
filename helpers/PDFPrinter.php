a5854079c1bedb8f10745e85796302502910c6d1
Ezequiel <71356016+exequii@users.noreply.github.com>
2021-11-03 17:35:52 -0300

    Actualizo la impresion de resumen reserva.

--- helpers/PDFPrinter.php
+++ helpers/PDFPrinter.php
@@ -8,11 +8,23 @@ class PDFPrinter
     {
 
     }
-    public function printPDF($data){
+    public function printPDF(){
+        $cantidad=$_GET['cantidad'];
+        $comprobante=$_GET['comprobante'];
+        $dia=$_GET['dia'];
+        $origen=$_GET['origen'];
+        $destino=$_GET['destino'];
+        $tipoVuelo=$_GET['tipo'];
         // instantiate and use the dompdf class
         $dompdf = new Dompdf();
         $dompdf->loadHtml(
-            '<h1>' . $data['nombre'] . '</h1>'
+            '<h1 style="border: 1px solid black; padding: 5px;">Resumen de su Reserva</h1><br>
+            <h3>- Cantidad de pasajes adquiridos: '.$cantidad.'</h3><br>
+            <h4>- Codigo de comprobante: ' .$comprobante. '</h4><br>
+            <h4>- Dia de Partida: ' .$dia.'</h4><br>
+            <h4>- Punto de Partida: ' .$origen. '</h4><br>
+            <h4>- Punto de Destino: ' .$destino. '</h4><br>
+            <h4>- Tipo de Vuelo: ' .$tipoVuelo. '</h4>'
         );
 
 //      (Optional) Setup the paper size and orientation
