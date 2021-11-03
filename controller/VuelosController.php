cc93ec2f5e1d46a6b81bad05ae6e63f50bfc48c2
Ezequiel <71356016+exequii@users.noreply.github.com>
2021-11-03 12:00:53 -0300

    Validacion reservar solo si esta logeado

--- controller/VuelosController.php
+++ controller/VuelosController.php
@@ -36,30 +36,29 @@ class VuelosController
     public function reservar(){
         $data['vuelo'] = $this->vuelosModel->buscarVueloPorId($_POST['vuelo']);
         $data['cantidad'] = $_POST['cantidad'];
-        $data['usuario'] = $_SESSION['usuario'];
-        //chequear nivel de usuario
-        $nivelUsuario = $data['usuario'][0]['nivelVuelo'];
-        $nivelVuelo= $data['vuelo'][0]['tipo_vuelo'];
-
-        //Preparacion de datos
-        $vuelo = $data['vuelo'][0]['idvuelo'];
-        $user = $data['usuario'][0]['idusuario'];
-        $equipo = $data['vuelo'][0]['equipo_id'];
-        $cantidad = $data['cantidad'];
-
-        
-        
-        if ($this->chequearNivelUsuario($nivelUsuario,$nivelVuelo)) {
-                $codigo = $this->generarCodigoComprobante("Boleto para ".$vuelo . "cantidad ".$cantidad);
-                $this->vuelosModel->realizarReserva($vuelo, $user, $cantidad,$codigo);
-                $this->vuelosModel->actualizarCapacidad($equipo, $cantidad);
-                $data['msg'] = "La reserva se ha realizado correctamente. Revise su correo.";
-                echo $this->printer->render( "view/vuelosDisponiblesView.html", $data);
-        } else {
-            $data['error'] = "Su nivel de vuelo no le permite viajar en este tipo de vuelo.";
+        if (!isset($_SESSION['usuario'])){
+            $data['error'] = "Debe iniciar sesion para poder reservar un vuelo.";
             echo $this->printer->render( "view/vuelosView.html", $data);
         }
-
+        else{
+            $data['usuario'] = $_SESSION['usuario'];
+            $nivelUsuario = $data['usuario'][0]['nivelVuelo'];
+            $nivelVuelo= $data['vuelo'][0]['tipo_vuelo'];
+            $vuelo = $data['vuelo'][0]['idvuelo'];
+            $user = $data['usuario'][0]['idusuario'];
+            $equipo = $data['vuelo'][0]['equipo_id'];
+            $cantidad = $data['cantidad'];
+            if ($this->chequearNivelUsuario($nivelUsuario,$nivelVuelo)) {
+                    $codigo = $this->generarCodigoComprobante("Boleto para ".$vuelo . "cantidad ".$cantidad);
+                    $this->vuelosModel->realizarReserva($vuelo, $user, $cantidad,$codigo);
+                    $this->vuelosModel->actualizarCapacidad($equipo, $cantidad);
+                    $data['msg'] = "La reserva se ha realizado correctamente. Revise su correo.";
+                    echo $this->printer->render( "view/vuelosDisponiblesView.html", $data);
+            } else {
+                $data['error'] = "Su nivel de vuelo no le permite viajar en este tipo de vuelo.";
+                echo $this->printer->render( "view/vuelosView.html", $data);
+            }
+        }
     }
 
      function chequearNivelUsuario($nivelUsuario,$nivelVuelo){
