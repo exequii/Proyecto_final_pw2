e2b1a678f57140ed32c96e3f71732b10e33e37f6
Nahuel Savedra <nsavedra97@gmail.com>
2021-11-04 12:36:14 -0300

    Agrego crear vuelos y modifico vista perfil Admin

--- controller/PerfilAdminController.php
+++ controller/PerfilAdminController.php
@@ -60,7 +60,38 @@ class PerfilAdminController{
         $data['usuario'] = $_SESSION['usuario'];
         $data['admin'] = $_SESSION['admin'];
         echo $this->printer->render( "view/perfilAdminView.html",$data);
-    }   
+    }
+    public function showCrearVuelo(){
+        $data['equipos'] = $this->perfilAdminModel->getEquipos();
+//        var_dump($data['equipos']);
+//        die();
+        if (isset($_SESSION['usuario'])){
+            $data['usuario'] = $_SESSION['usuario'];
+            if (isset($_SESSION['admin'])){
+                $data['admin'] = $_SESSION['admin'];
+            }
+            echo $this->printer->render( "view/admin/crearVueloView.html",$data);
+        }else {
+            echo $this->printer->render( "view/admin/crearVueloView.html");
+        }
+    }
+
+    public function guardarVuelo(){
+        $data['vuelo'] = $_POST;
+        $dia=$data['vuelo']['dia'];
+        $equipo_id=$data['vuelo']['equipo'];
+        $duracion=$data['vuelo']['duracion'];
+        $partida=$data['vuelo']['partida'];
+        $destino=$data['vuelo']['destino'];
+        $tipo_vuelo=$data['vuelo']['tipo_vuelo'];
+        $horario=$data['vuelo']['horario'];
+//
+//        var_dump($data['vuelo']);
+//        die();
+        $this->perfilAdminModel->insertVueloSemanal($dia,$equipo_id,$duracion,$partida,$destino,$tipo_vuelo,$horario);
+
+        header("Location: /view/perfilAdminView.html");
+    }
 }
 
 ?>
--- model/PerfilAdminModel.php
+++ model/PerfilAdminModel.php
@@ -9,8 +9,19 @@ class PerfilAdminModel
     }
 
     function getTurnos(){
-        $sql = "SELECT * FROM turno";
+        $sql = "SELECT usuario.usuario, hospital.nombre, turno.dia, usuario.nivelVuelo 
+                        FROM turno JOIN hospital ON turno.hospital_id = hospital.idhospital 
+                                    JOIN usuario ON turno.usuario_id = usuario.idusuario";
         return $this->database->query($sql);
+    }
+    function getEquipos(){
+        $sql = "SELECT idequipo,modelo,matricula FROM equipo";
+        return $this->database->query($sql);
+    }
+    function insertVueloSemanal($dia,$equipo_id,$duracion,$partida,$destino,$tipo_vuelo,$horario){
+        $sql = "INSERT INTO `vuelo_semanal` (`idvuelo_semanal`, `dia`, `equipo_id`, `duracion`, `partida`, `destino`, `tipo_vuelo`, `horario`) 
+        VALUES (NULL, '" . $dia ."', '".$equipo_id ."', '".$duracion."', '".$partida."', '".$destino."', '".$tipo_vuelo."', '".$horario."')";
+        $this->database->insertQuery($sql);
     }
 }
 
